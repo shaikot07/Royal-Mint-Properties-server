de08e42c05d375583058ca33e460b1c90a20fb71
shaikot07 <saifulislamshaikat007@gmail.com>
2024-09-10 04:07:31 +0600

    server setup and connect with mongoDB database

---
+++ index.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middlewer
+app.use(cors());
+app.use(express.json());
+
+
+
+// console.log(process.env.DB_user);
+// console.log(process.env.DB_pass);
+const uri = `mongodb+srv://${process.env.DB_user}:${process.env.DB_pass}@learnmongo.htgdx.mongodb.net/?retryWrites=true&w=majority&appName=learnMongo`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        // our Custom code start for crud 
+        const userCollection = client.db('Royal-Mint-Properties').collection('users');
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            // insert email if user  dose't exists 
+            const query = { email: user.email }
+            const existingUser = await userCollection.findOne(query);
+            if (existingUser) {
+                  return res.send({ message: 'User already exists', insertedId: null })
+            }
+            const result = await userCollection.insertOne(user);
+            res.send(result);
+      })
+
+         // our Custom code end for crud 
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('simple CRUD server is RUNNING')
+})
+
+app.listen(port, () => {
+    console.log(`simple CRUD is Running on port,${port}`);
+})
+
+
+
+
+
