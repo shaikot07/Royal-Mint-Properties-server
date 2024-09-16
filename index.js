2c3a1b4678293b9b9cdf60aec87d0407b51f1848
Dodul01 <mhdodul299@gmail.com>
2024-09-17 01:11:42 +0600

    JWT Middleware added

--- index.js
+++ index.js
@@ -4,16 +4,36 @@ const cors = require('cors');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
+const jwt = require('jsonwebtoken')
 
 // middlewer
 app.use(cors());
 app.use(express.json());
 
-
-
-// console.log(process.env.DB_user);
-// console.log(process.env.DB_pass);
 const uri = `mongodb+srv://${process.env.DB_user}:${process.env.DB_pass}@learnmongo.htgdx.mongodb.net/?retryWrites=true&w=majority&appName=learnMongo`;
+
+// VerifyToken middleware
+const verifyToken = (req, res, next) => {
+    const token = req.headers.authorization && req.headers.authorization.split(' ')[1];
+
+    if (!token) {
+        return res.status(401).send({ message: 'Unauthorized' });
+    }
+
+    jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoad) => {
+        if (err) {
+            return res.status(401).send({ message: 'Unauthorized' })
+        }
+
+        req.decode = decoad;
+        const email = decoad.email;
+        req.role = email;
+
+        next();
+    })
+}
+
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -38,13 +58,18 @@ async function run() {
             const query = { email: user.email }
             const existingUser = await userCollection.findOne(query);
             if (existingUser) {
-                  return res.send({ message: 'User already exists', insertedId: null })
+                return res.send({ message: 'User already exists', insertedId: null })
             }
             const result = await userCollection.insertOne(user);
             res.send(result);
-      })
+        })
 
-         // our Custom code end for crud 
+        app.get('/users', async (req, res) => {
+            const result = await userCollection.find().toArray();
+            res.send(result);
+        })
+
+        // our Custom code end for crud 
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
