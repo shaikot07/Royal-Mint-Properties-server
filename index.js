3be6b75bceb06b66e005d61282b9feb43915f34e
Dodul01 <mhdodul299@gmail.com>
2024-09-18 00:38:19 +0600

    Server updated

--- index.js
+++ index.js
@@ -139,8 +139,8 @@ async function run() {
         })
 
         // our Custom code end for crud 
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        // await client.db("admin").command({ ping: 1 });
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
